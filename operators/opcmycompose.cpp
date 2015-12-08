413eeb893d6ab8f0f59bd9e71565a120eb520c04
Guillaume Gimenez <ploki@blackmilk.fr>
2015-12-09 00:27:30 +0100

    wip cmy compose

--- operators/opcmycompose.cpp
+++ operators/opcmycompose.cpp
@@ -5,6 +5,12 @@
 #include "cielab.h"
 #include "igamma.h"
 
+static Photo
+blackDot()
+{
+    return Photo(Magick::Image(Magick::Geometry(1,1), Magick::Color(0,0,0)), Photo::Linear);
+}
+
 class WorkerCMYCompose : public OperatorWorker {
 public:
     WorkerCMYCompose(QThread *thread, Operator *op) :
@@ -14,57 +20,85 @@ public:
         throw 0;
     }
     void play() {
-        int lab_count = m_inputs[0].count();
-        int photo_count = m_inputs[1].count();
-        for (int i = 2 ; i < 4 ; ++i )
-            if ( m_inputs[i].count() != photo_count ) {
-                qWarning("Uneven photo count in LCMY Compose");
-                emitFailure();
-                return;
-            }
-        if ( lab_count && lab_count != photo_count ) {
-            qWarning("Wrong number of Luminance photos in LCMY Compose");
-            emitFailure();
-            return;
-        }
+        int l_count = m_inputs[0].count();
+        int c_count = m_inputs[1].count();
+        int m_count = m_inputs[2].count();
+        int y_count = m_inputs[3].count();
+        int photo_count = qMax(qMax(qMax(l_count, c_count), m_count), y_count);
+
         for( int i = 0 ; i < photo_count ; ++i ) {
             if ( aborted() )
                 continue;
-            Photo pCyan(m_inputs[1][i]);
-            Photo pMagenta(m_inputs[2][i]);
-            Photo pYellow(m_inputs[3][i]);
+            Photo pLuminance;
+            Photo pCyan;
+            Photo pMagenta;
+            Photo pYellow;
+            if ( l_count )
+                pLuminance = m_inputs[0][i%l_count];
+            else
+                pLuminance = blackDot();
+
+            if ( c_count )
+                pCyan = m_inputs[1][i%c_count];
+            else
+                pCyan = blackDot();
+
+            if ( m_count )
+                pMagenta = m_inputs[2][i%m_count];
+            else
+                pMagenta = blackDot();
+
+            if ( y_count )
+                pYellow = m_inputs[3][i%y_count];
+            else
+                pYellow = blackDot();
+
+            Magick::Image& iLuminance = pLuminance.image();
             Magick::Image& iCyan = pCyan.image();
             Magick::Image& iMagenta = pMagenta.image();
             Magick::Image& iYellow = pYellow.image();
-            iCyan.modifyImage();
+
+            int w = qMax(qMax(qMax(iLuminance.columns(),iCyan.columns()), iMagenta.columns()), iYellow.columns());
+            int h = qMax(qMax(qMax(iLuminance.rows(),iCyan.rows()), iMagenta.rows()), iYellow.rows());
+            iLuminance.extent(Magick::Geometry(w, h), Magick::Color(0,0,0), Magick::NorthWestGravity);
+            iCyan.extent(Magick::Geometry(w, h), Magick::Color(0,0,0), Magick::NorthWestGravity);
+            iMagenta.extent(Magick::Geometry(w, h), Magick::Color(0,0,0), Magick::NorthWestGravity);
+            iYellow.extent(Magick::Geometry(w, h), Magick::Color(0,0,0), Magick::NorthWestGravity);
+
             Magick::Pixels iCyan_cache(iCyan);
             Magick::Pixels iMagenta_cache(iMagenta);
             Magick::Pixels iYellow_cache(iYellow);
-            int w = iCyan.columns();
-            int h = iCyan.rows();
+
+
+            Photo photo(Photo::Linear);
+            photo.createImage(w, h);
+            photo.setSequenceNumber(i);
+            photo.setIdentity(m_operator->uuid() + ":" + QString::number(i));
+            photo.setTag("Name", "LCMY Composition");
+            Magick::Pixels iPhoto_cache(photo.image());
+            Magick::PixelPacket *pxl = iPhoto_cache.get(0, 0, w, h);
 #pragma omp parallel for
             for ( int y = 0 ; y < h ; ++y ) {
-                Magick::PixelPacket *pxl_Cyan = iCyan_cache.get(0, y, w, 1);
+                const Magick::PixelPacket *pxl_Cyan = iCyan_cache.getConst(0, y, w, 1);
                 const Magick::PixelPacket *pxl_Magenta = iMagenta_cache.getConst(0, y, w, 1);
                 const Magick::PixelPacket *pxl_Yellow = iYellow_cache.getConst(0, y, w, 1);
                 for ( int x = 0 ; x < w ; ++x ) {
                     quantum_t rgb[3];
-                    rgb[0] =  - pxl_Cyan[x].red + pxl_Magenta[x].green + pxl_Yellow[x].blue;
-                    rgb[1] =    pxl_Cyan[x].red - pxl_Magenta[x].green + pxl_Yellow[x].blue;
-                    rgb[2] =    pxl_Cyan[x].red + pxl_Magenta[x].green - pxl_Yellow[x].blue;
-                    pxl_Cyan[x].red = clamp(rgb[0]);
-                    pxl_Cyan[x].green = clamp(rgb[1]);
-                    pxl_Cyan[x].blue = clamp(rgb[2]);
+                    rgb[0] =  - (pxl_Cyan?pxl_Cyan[x].red:0) + (pxl_Magenta?pxl_Magenta[x].green:0) + (pxl_Yellow?pxl_Yellow[x].blue:0);
+                    rgb[1] =    (pxl_Cyan?pxl_Cyan[x].red:0) - (pxl_Magenta?pxl_Magenta[x].green:0) + (pxl_Yellow?pxl_Yellow[x].blue:0);
+                    rgb[2] =    (pxl_Cyan?pxl_Cyan[x].red:0) + (pxl_Magenta?pxl_Magenta[x].green:0) - (pxl_Yellow?pxl_Yellow[x].blue:0);
+                    pxl[y*w+x].red = clamp(rgb[0]);
+                    pxl[y*w+x].green = clamp(rgb[1]);
+                    pxl[y*w+x].blue = clamp(rgb[2]);
                 }
             }
-            iCyan_cache.sync();
-            if ( lab_count ) {
-                Photo lPhoto(m_inputs[0][i]);
+            iPhoto_cache.sync();
+            if ( l_count ) {
                 iGamma& labGamma = iGamma::Lab();
-                labGamma.applyOn(lPhoto);
-                unLabize(pCyan.image(), lPhoto.image());
+                labGamma.applyOn(pLuminance);
+                unLabize(photo.image(), pLuminance.image());
             }
-            outputPush(0, pCyan);
+            outputPush(0, photo);
             emitProgress(i, photo_count, 1, 1);
         }
         if (aborted())

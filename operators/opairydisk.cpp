ef01fe8f275acd1d5ee083d37274a01339b065ce
Guillaume Gimenez <ploki@blackmilk.fr>
2016-09-05 02:24:08 +0200

    normalize Airy disk luminosity

--- operators/opairydisk.cpp
+++ operators/opairydisk.cpp
@@ -1,4 +1,33 @@
-#include "opairydisk.h"
+/*
+ * Copyright (c) 2006-2016, Guillaume Gimenez <guillaume@blackmilk.fr>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of G.Gimenez nor the names of its contributors may
+ *       be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL G.Gimenez BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors:
+ *     * Guillaume Gimenez <guillaume@blackmilk.fr>
+ *
+ */#include "opairydisk.h"
 #include "operatoroutput.h"
 #include "operatorworker.h"
 #include "photo.h"
@@ -51,17 +80,13 @@ private slots:
             std::shared_ptr<Ordinary::Pixels> cache(new Ordinary::Pixels(image));
             int w = image.columns();
             int h = image.rows();
-            dfl_parallel_for(y, 0, h, 4, (image), {
+            double *pixels = new double[w*h*3];
+            double max = 0;
+            int sq_p = m_precision*m_precision;
+            dfl_parallel_for(y, 0, h, 4, (), {
                 if (aborted())
                     continue;
-                emit progress(y, h);
-                Magick::PixelPacket *pixels = cache->get(0,y,w,1);
-                if ( m_error || !pixels ) {
-                    if ( !m_error )
-                        dflError(DF_NULL_PIXELS);
-                    m_error = true;
-                    continue;
-                }
+                emit progress(y/2, h);
                 for (int x = 0 ; x < w ; ++x ) {
                     double rgb[3] = {0, 0, 0};
                     for ( int jy = 0 ; jy < m_precision ; ++jy ) {
@@ -79,13 +104,36 @@ private slots:
                             }
                         }
                     }
-                    int sq_p = m_precision*m_precision;
-                    pixels[x].red = clamp<quantum_t>(rgb[0] / (sq_p));
-                    pixels[x].green = clamp<quantum_t>(rgb[1] / (sq_p));
-                    pixels[x].blue = clamp<quantum_t>(rgb[2] / (sq_p));
+                    rgb[0]/=sq_p;
+                    rgb[1]/=sq_p;
+                    rgb[2]/=sq_p;
+                    pixels[y*w*3+x*3+0] = rgb[0];
+                    pixels[y*w*3+x*3+1] = rgb[1];
+                    pixels[y*w*3+x*3+2] = rgb[2];
+                    max = qMax(max,qMax(rgb[0],qMax(rgb[1], rgb[2])));
+                }
+
+            });
+            double cor = double(QuantumRange)/max;
+            dfl_parallel_for(y, 0, h, 4, (image), {
+                if (aborted())
+                    continue;
+                emit progress(h/2+y/2, h);
+                Magick::PixelPacket *pxl = cache->get(0,y,w,1);
+                if ( m_error || !pxl ) {
+                    if ( !m_error )
+                        dflError(DF_NULL_PIXELS);
+                    m_error = true;
+                    continue;
+                }
+                for (int x = 0 ; x < w ; ++x ) {
+                    pxl[x].red = clamp<quantum_t>(cor * pixels[y*w*3+x*3+0]);
+                    pxl[x].green = clamp<quantum_t>(cor * pixels[y*w*3+x*3+1]);
+                    pxl[x].blue = clamp<quantum_t>(cor * pixels[y*w*3+x*3+2]);
                 }
                 cache->sync();
             });
+            delete[] pixels;
             if (aborted()) {
                 emitFailure();
             }

362f8795801d56469a93153a4cb5c8b2adbf771e
Guillaume Gimenez <ploki@blackmilk.fr>
2016-09-17 22:12:54 +0200

    lcmy compose, alignment and HDR

--- operators/opcmycompose.cpp
+++ operators/opcmycompose.cpp
@@ -32,20 +32,19 @@
 #include "operatorworker.h"
 #include "operatorinput.h"
 #include "operatoroutput.h"
+#include "operatorparameterdropdown.h"
 #include "algorithm.h"
 #include "console.h"
 #include "cielab.h"
-
-static Photo
-blackDot()
-{
-    return Photo(Magick::Image(Magick::Geometry(1,1), Magick::Color(0,0,0)), Photo::Linear);
-}
+#include "hdr.h"
+#include "transformview.h"
 
 class WorkerCMYCompose : public OperatorWorker {
+    bool m_outputHDR;
 public:
-    WorkerCMYCompose(QThread *thread, Operator *op) :
-        OperatorWorker(thread, op)
+    WorkerCMYCompose(bool outputHDR, QThread *thread, Operator *op) :
+        OperatorWorker(thread, op),
+        m_outputHDR(outputHDR)
     {}
     Photo process(const Photo &, int , int ) {
         throw 0;
@@ -64,96 +63,144 @@ public:
             Photo pCyan;
             Photo pMagenta;
             Photo pYellow;
+            Photo *photos[4] = {};
+            std::shared_ptr<TransformView> tvLuminance(0);
+            std::shared_ptr<TransformView> tvCyan(0);
+            std::shared_ptr<TransformView> tvMagenta(0);
+            std::shared_ptr<TransformView> tvYellow(0);
+            unsigned w = 0, h = 0;
+            QVector<QPointF> reference;
 
-            if ( l_count )
+            if ( l_count ) {
                 pLuminance = m_inputs[0][i%l_count];
-            else
-                pLuminance = blackDot();
+                photos[0] = &pLuminance;
+            }
 
-            if ( c_count )
+            if ( c_count ) {
                 pCyan = m_inputs[1][i%c_count];
-            else
-                pCyan = blackDot();
+                photos[2] = &pCyan;
+            }
 
-            if ( m_count )
+            if ( m_count ) {
                 pMagenta = m_inputs[2][i%m_count];
-            else
-                pMagenta = blackDot();
+                photos[3] = &pMagenta;
+            }
 
-            if ( y_count )
+            if ( y_count ) {
                 pYellow = m_inputs[3][i%y_count];
-            else
-                pYellow = blackDot();
+                photos[1] = &pYellow;
+            }
 
-            Magick::Image& iLuminance = pLuminance.image();
-            Magick::Image& iCyan = pCyan.image();
-            Magick::Image& iMagenta = pMagenta.image();
-            Magick::Image& iYellow = pYellow.image();
-
-            unsigned w = qMax(qMax(qMax(iLuminance.columns(),iCyan.columns()), iMagenta.columns()), iYellow.columns());
-            unsigned h = qMax(qMax(qMax(iLuminance.rows(),iCyan.rows()), iMagenta.rows()), iYellow.rows());
-            if ( iLuminance.columns() != w || iLuminance.rows() != h )
-                iLuminance.extent(Magick::Geometry(w, h), Magick::Color(0,0,0), Magick::NorthWestGravity);
-            if ( iCyan.columns() != w || iCyan.rows() != h )
-                iCyan.extent(Magick::Geometry(w, h), Magick::Color(0,0,0), Magick::NorthWestGravity);
-            if ( iMagenta.columns() != w || iMagenta.rows() != h )
-                iMagenta.extent(Magick::Geometry(w, h), Magick::Color(0,0,0), Magick::NorthWestGravity);
-            if ( iYellow.columns() != w || iYellow.rows() != h )
-                iYellow.extent(Magick::Geometry(w, h), Magick::Color(0,0,0), Magick::NorthWestGravity);
+            Photo *refPhoto = Photo::findReference(photos, sizeof(photos)/sizeof(*photos));
+            if (refPhoto) {
+                reference = refPhoto->getPoints();
+                w = refPhoto->image().columns();
+                h = refPhoto->image().rows();
+            }
+            if ( l_count ) {
+                tvLuminance.reset(new TransformView(pLuminance, 1, reference));
+                if ( tvLuminance->inError() || !tvLuminance->loadPixels()) {
+                    m_error = true;
+                    continue;
+                }
+            }
+            if ( c_count ) {
+                tvCyan.reset(new TransformView(pCyan, 1, reference));
+                if ( tvCyan->inError() || !tvCyan->loadPixels()) {
+                    m_error = true;
+                    continue;
+                }
+            }
+            if ( m_count ) {
+                tvMagenta.reset(new TransformView(pMagenta, 1, reference));
+                if ( tvMagenta->inError() || !tvMagenta->loadPixels()) {
+                    m_error = true;
+                    continue;
+                }
+            }
+            if ( y_count ) {
+                tvYellow.reset(new TransformView(pYellow, 1, reference));
+                if ( tvYellow->inError() || !tvYellow->loadPixels()) {
+                    m_error = true;
+                    continue;
+                }
+            }
 
             try {
-                std::shared_ptr<Ordinary::Pixels> iCyan_cache(new Ordinary::Pixels(iCyan));
-                std::shared_ptr<Ordinary::Pixels> iMagenta_cache(new Ordinary::Pixels(iMagenta));
-                std::shared_ptr<Ordinary::Pixels> iYellow_cache(new Ordinary::Pixels(iYellow));
-                std::shared_ptr<Ordinary::Pixels> iLuminance_cache(new Ordinary::Pixels(iLuminance));
-
-
                 Photo photo(Photo::Linear);
                 photo.createImage(w, h);
                 photo.setSequenceNumber(i);
                 photo.setIdentity(m_operator->uuid() + ":" + QString::number(i));
                 photo.setTag(TAG_NAME, "LCMY Composition");
-                ResetImage(photo.image());
                 Ordinary::Pixels iPhoto_cache(photo.image());
                 Magick::PixelPacket *pxl = iPhoto_cache.get(0, 0, w, h);
                 dfl_block int line = 0;
-                dfl_parallel_for(y, 0, int(h), 4, (iCyan, iMagenta, iYellow, iLuminance), {
-                    const Magick::PixelPacket *pxl_Cyan = iCyan_cache->getConst(0, y, w, 1);
-                    const Magick::PixelPacket *pxl_Magenta = iMagenta_cache->getConst(0, y, w, 1);
-                    const Magick::PixelPacket *pxl_Yellow = iYellow_cache->getConst(0, y, w, 1);
-                    const Magick::PixelPacket *pxl_Luminance = iLuminance_cache->getConst(0, y, w, 1);
-                    if ( m_error || !pxl_Cyan || !pxl_Magenta || !pxl_Yellow || !pxl_Luminance ) {
-                        if (!m_error)
-                            dflError(DF_NULL_PIXELS);
+                bool hdrLuminance = pLuminance.getScale() == Photo::HDR;
+                bool hdrCyan = pCyan.getScale() == Photo::HDR;
+                bool hdrMagenta = pMagenta.getScale() == Photo::HDR;
+                bool hdrYellow = pYellow.getScale() == Photo::HDR;
+                dfl_parallel_for(y, 0, int(h), 4, (), {
+                    if ( m_error ) {
                         continue;
                     }
                     for ( unsigned x = 0 ; x < w ; ++x ) {
-                        quantum_t rgb[3];
-                        quantum_t cyan = 0;
-                        quantum_t magenta = 0;
-                        quantum_t yellow = 0;
-                        if ( pxl_Cyan ) cyan = (pxl_Cyan[x].green+pxl_Cyan[x].blue)/2;
-                        if ( pxl_Magenta ) magenta = (pxl_Magenta[x].red+pxl_Magenta[x].blue)/2;
-                        if ( pxl_Yellow) yellow = (pxl_Yellow[x].red+pxl_Yellow[x].green)/2;
+                        double rgb[3];
+                        double cyan=0, magenta=0, yellow=0;
+                        if (tvCyan) {
+                            Magick::PixelPacket pxl = tvCyan->getPixel(x, y, 0);
+                            if (hdrCyan) {
+                                cyan = fromHDR(pxl.green) + fromHDR(pxl.blue) / 2.;
+                            }
+                            else {
+                                cyan = double(pxl.green) + double(pxl.blue) / 2.;
+                            }
+                        }
+                        if (tvMagenta) {
+                            Magick::PixelPacket pxl = tvMagenta->getPixel(x, y, 0);
+                            if (hdrMagenta) {
+                                magenta = fromHDR(pxl.red) + fromHDR(pxl.blue) / 2.;
+                            }
+                            else {
+                                magenta = double(pxl.red) + double(pxl.blue) / 2.;
+                            }
+                        }
+                        if (tvYellow) {
+                            Magick::PixelPacket pxl = tvYellow->getPixel(x, y, 0);
+                            if (hdrYellow) {
+                                yellow = fromHDR(pxl.green) + fromHDR(pxl.red) / 2.;
+                            }
+                            else {
+                                yellow = double(pxl.green) + double(pxl.red) / 2.;
+                            }
+                        }
 
                         rgb[0] =  - cyan + magenta + yellow;
                         rgb[1] =    cyan - magenta + yellow;
                         rgb[2] =    cyan + magenta - yellow;
-                        if ( l_count ) {
-                            double lum = LUMINANCE(pxl_Luminance?pxl_Luminance[x].red:0,
-                                                   pxl_Luminance?pxl_Luminance[x].green:0,
-                                                   pxl_Luminance?pxl_Luminance[x].blue:0);
+
+                        if (tvLuminance) {
+                            Magick::PixelPacket pixel = tvLuminance->getPixel(x, y, 0);
+                            double lum = LUMINANCE(hdrLuminance?fromHDR(pixel.red):pixel.red,
+                                                   hdrLuminance?fromHDR(pixel.green):pixel.green,
+                                                   hdrLuminance?fromHDR(pixel.blue):pixel.blue);
                             double cur = LUMINANCE(rgb[0],
                                                    rgb[1],
                                                    rgb[2]);
                             double mul = lum/cur;
-                            rgb[0] = DF_ROUND(mul*rgb[0]);
-                            rgb[1] = DF_ROUND(mul*rgb[1]);
-                            rgb[2] = DF_ROUND(mul*rgb[2]);
+                            rgb[0] = mul*rgb[0];
+                            rgb[1] = mul*rgb[1];
+                            rgb[2] = mul*rgb[2];
                         }
-                        pxl[y*w+x].red = clamp(rgb[0]);
-                        pxl[y*w+x].green = clamp(rgb[1]);
-                        pxl[y*w+x].blue = clamp(rgb[2]);
+                        if (m_outputHDR) {
+                            pxl[y*w+x].red = toHDR(rgb[0]);
+                            pxl[y*w+x].green = toHDR(rgb[1]);
+                            pxl[y*w+x].blue = toHDR(rgb[2]);
+                        }
+                        else {
+                            pxl[y*w+x].red=clamp<quantum_t>(DF_ROUND(rgb[0]));
+                            pxl[y*w+x].green=clamp<quantum_t>(DF_ROUND(rgb[1]));
+                            pxl[y*w+x].blue=clamp<quantum_t>(DF_ROUND(rgb[2]));
+                        }
                     }
                     dfl_critical_section(
                     {
@@ -161,6 +208,8 @@ public:
                     });
                 });
                 iPhoto_cache.sync();
+                if (m_outputHDR)
+                    photo.setScale(Photo::HDR);
                 outputPush(0, photo);
                 emitProgress(i, photo_count, 1, 1);
             }
@@ -185,13 +234,19 @@ public:
 };
 
 OpCMYCompose::OpCMYCompose(Process *parent) :
-    Operator(OP_SECTION_COLOR, QT_TRANSLATE_NOOP("Operator", "LCMY Compose"), Operator::NonHDR, parent)
+    Operator(OP_SECTION_COLOR, QT_TRANSLATE_NOOP("Operator", "LCMY Compose"), Operator::NonHDR, parent),
+    m_outputHDR(new OperatorParameterDropDown("outputHDR", tr("Output HDR"), this, SLOT(setOutputHDR(int)))),
+    m_outputHDRValue(false)
 {
     addInput(new OperatorInput(tr("Luminance"), OperatorInput::Set, this));
     addInput(new OperatorInput(tr("Cyan"), OperatorInput::Set, this));
     addInput(new OperatorInput(tr("Magenta"), OperatorInput::Set, this));
     addInput(new OperatorInput(tr("Yellow"), OperatorInput::Set, this));
     addOutput(new OperatorOutput(tr("RGB"), this));
+
+    m_outputHDR->addOption(DF_TR_AND_C("No"), false, true);
+    m_outputHDR->addOption(DF_TR_AND_C("Yes"), true);
+    addParameter(m_outputHDR);
 }
 
 OpCMYCompose *OpCMYCompose::newInstance()
@@ -201,5 +256,13 @@ OpCMYCompose *OpCMYCompose::newInstance()
 
 OperatorWorker *OpCMYCompose::newWorker()
 {
-    return new WorkerCMYCompose(m_thread, this);
+    return new WorkerCMYCompose(m_outputHDRValue, m_thread, this);
+}
+
+void OpCMYCompose::setOutputHDR(int type)
+{
+    if ( m_outputHDRValue != !!type ) {
+        m_outputHDRValue = !!type;
+        setOutOfDate();
+    }
 }
